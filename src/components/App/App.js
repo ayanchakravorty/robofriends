b0350714f939c487e4d8cbb3114685b482adcc00
Ayan Chakravorty <ayanchakravorty@gmail.com>
2022-02-05 17:03:54 +0530

    redux implementation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
-import React,{ useEffect, useState } from 'react';
+import React,{ useEffect } from 'react';
+import { connect } from 'react-redux';
 import RobotsList from '../RobotsList/RobotsList';
 import SearchBox from '../Utility/SearchBox';
 import Scroll from '../Utility/Scroll';
@@ -6,36 +7,51 @@ import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 import './App.css';
 
-export const App = () => {
-    const [robots, setRobots] = useState([]);
-    const [searchText, setSearchText] = useState('');
+import { setSearchText, requestRobots } from '../../actions';
 
+const mapStateToProps = (state) => ({
+    searchText: state.searchRobots.searchText,
+    robots: state.requestRobots.robots,
+    error: state.requestRobots.error,
+    isPending: state.requestRobots.isPending,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    onSearch: (event) => dispatch(setSearchText(event.target.value.toLowerCase())),
+    onRequestRobots: () => dispatch(requestRobots())
+})
+
+export const App = (props) => {
+    // const [robots, setRobots] = useState([]);
+    // const [searchText, setSearchText] = useState('');
+    const funcCall = () => {
+        props.onRequestRobots();
+    }
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((data) =>
-            setRobots(data)
-        );
+        funcCall();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const onSearch = (event) =>
-        setSearchText(event.target.value.toLowerCase());
+    // const onSearch = (event) =>
+    //     setSearchText(event.target.value.toLowerCase());
 
-    const filteredRobots = robots.filter((robot) => 
-        robot.name.toLowerCase().includes(searchText.toLowerCase())
+    const filteredRobots = props.robots.filter((robot) => 
+        robot.name.toLowerCase().includes(props.searchText.toLowerCase())
     );
 
-    return robots.length === 0 ? (
+    return props.isPending ? (
         <h1>Loading...</h1>
         ) : (
         <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
-            <SearchBox onSearch={onSearch} />
+            <SearchBox onSearch={props.onSearch} />
             <Scroll>
                 <ErrorBoundary>
-                    <RobotsList robots={filteredRobots} searchText={searchText} />
+                    <RobotsList robots={filteredRobots} searchText={props.searchText} />
                 </ErrorBoundary>
             </Scroll>
         </div>
     );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
