8ee268e315a4d574214ad773055b8cd96a4ac4b5
Ayan Chakravorty <ayanchakravorty@gmail.com>
2022-02-05 13:23:20 +0530

    hooks added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React,{ useEffect, useState } from 'react';
 import RobotsList from '../RobotsList/RobotsList';
 import SearchBox from '../Utility/SearchBox';
 import Scroll from '../Utility/Scroll';
@@ -6,46 +6,36 @@ import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchText: '',
-        }
-    }
+export const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchText, setSearchText] = useState('');
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((data) => this.setState({
-            robots: data
-        }))
-    }
-
-    onSearch = (event) => {
-        this.setState({
-            searchText: event.target.value.toLowerCase(),
-        });
-    }
-
-    render() {
-        const { robots, searchText } = this.state;
-        const filteredRobots = robots.filter((robot) => 
-            robot.name.toLowerCase().includes(searchText.toLowerCase())
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((data) =>
+            setRobots(data)
         );
-        return robots.length === 0 ? (
-            <h1>Loading...</h1>
-            ) : (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox onSearch={this.onSearch} />
-                <Scroll>
-                    <ErrorBoundary>
-                        <RobotsList robots={filteredRobots} searchText={this.state.searchText} />
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        )
-    }
+    }, []);
+
+    const onSearch = (event) =>
+        setSearchText(event.target.value.toLowerCase());
+
+    const filteredRobots = robots.filter((robot) => 
+        robot.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    return robots.length === 0 ? (
+        <h1>Loading...</h1>
+        ) : (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox onSearch={onSearch} />
+            <Scroll>
+                <ErrorBoundary>
+                    <RobotsList robots={filteredRobots} searchText={searchText} />
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    );
 }
 
 export default App;
